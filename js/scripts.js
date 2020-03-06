30606ee83e6fbb2e5be223c3af3b1f461e98027e
Student <example@example.com>
2020-03-05 20:47:49 -0800

    Started trying to build out functionality, but ran aground at trying to target one-letter vowel words.

--- js/scripts.js
+++ js/scripts.js
@@ -14,8 +14,12 @@ $(document).ready(function() {
     englishWords.forEach(function(englishWord){
       if (englishWord.match(/[0-9]/)) {
         console.log("integer detected")
+      } else if (englishWord.match("a","A","e","E","i","I","o","O","u","U")) {
+        englishWord.concat("ay");
+        console.log("What? only if it's the whole word" + englishWord.concat("ay"));
       } else if (englishWord.charAt(0).match(/[aeiouAEIOU]/)) {
-        console.log("add 'way' to the end of that word");
+        englishWord.concat("way");
+        console.log(englishWord.concat("way"));
       } else if (englishWord.charAt(0).match(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/)) {
         console.log("This word needs to be pig-latinized")
       }
